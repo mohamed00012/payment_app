3f8bb95554b0d82cc5b2f6f7501277bc59eae6dd
mohamed00012 <52926834+mohamed00012@users.noreply.github.com>
2022-10-24 19:27:03 +0200

    Add files via upload

--- payment_application/card/card.h
+++ payment_application/card/card.h
@@ -1,16 +1,16 @@
 #ifndef CARD_H_
 #define CARD_H_
+#include <stdint.h>
+#include <stdio.h>
 
-
-
-typedef struct ST_cardData_t
+typedef struct 
 {
     uint8_t cardHolderName[25];
     uint8_t primaryAccountNumber[20];
     uint8_t cardExpirationDate[6];
 }ST_cardData_t;
 
-typedef enum EN_cardError_t
+typedef enum 
 {
     CARD_OK, WRONG_NAME, WRONG_EXP_DATE, WRONG_PAN
 }EN_cardError_t;
